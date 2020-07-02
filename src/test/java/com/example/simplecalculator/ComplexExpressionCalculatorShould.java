2fd59746be30faa284c3a98fd11af25f0f4f044c
Muhammad Umer <umermuhammadkhan@gmail.com>
2020-07-02 08:05:34 +0400

    Refactor POM

--- src/test/java/com/example/simplecalculator/ComplexExpressionCalculatorShould.java
+++ src/test/java/com/example/simplecalculator/ComplexExpressionCalculatorShould.java
@@ -1,15 +1,19 @@
 package com.example.simplecalculator;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class ComplexExpressionCalculatorShould {
+public class ComplexExpressionCalculatorShould {
 
     @Autowired
     private Calculator calculator;
